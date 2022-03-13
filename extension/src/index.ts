590673960d122965eddfb37705afc40bf6e795dd
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-14 07:28:02 +0800

    feat: prompt for update

--- extension/src/index.ts
+++ extension/src/index.ts
@@ -2,6 +2,15 @@ import fetch from 'node-fetch'
 import type { ExtensionContext } from 'vscode'
 import { window, workspace } from 'vscode'
 
+/**
+ * TODO:
+ *
+ * - [x] prompt about overriding
+ * - cache
+ * - settings (interval)
+ * - manual command
+ */
+
 const URL = 'https://cdn.jsdelivr.net/gh/antfu/vscode-file-nesting-config@main/README.md'
 
 async function fetchLatest() {
@@ -22,19 +31,34 @@ async function fetchLatest() {
 }
 
 export async function activate(ctx: ExtensionContext) {
-  window.showInformationMessage('Hello from file-nesting-config!')
+  fetchAndUpdate(true)
+}
 
+async function fetchAndUpdate(prompt = true) {
   const patterns = await fetchLatest()
+  let shouldUpdate = true
 
-  const config = workspace.getConfiguration()
-  if (!config.has('explorer.experimental.fileNesting.enabled'))
-    config.update('explorer.experimental.fileNesting.enabled', true, true)
-  if (!config.has('explorer.experimental.fileNesting.expand'))
-    config.update('explorer.experimental.fileNesting.expand', false, true)
+  if (prompt) {
+    const buttonUpdate = 'Update'
+    const buttonSkip = 'Skip this time'
+    const result = await window.showInformationMessage(
+      '[File Nesting] new config found, do you want to update?',
+      buttonUpdate,
+      buttonSkip,
+    )
+    shouldUpdate = result === buttonUpdate
+  }
 
-  // TODO: prompt about overriding
-  config.update('explorer.experimental.fileNesting.patterns', patterns, true)
-  window.showInformationMessage('Updated with latest patterns')
+  if (shouldUpdate) {
+    const config = workspace.getConfiguration()
+    if (!config.has('explorer.experimental.fileNesting.enabled'))
+      config.update('explorer.experimental.fileNesting.enabled', true, true)
+    if (!config.has('explorer.experimental.fileNesting.expand'))
+      config.update('explorer.experimental.fileNesting.expand', false, true)
+    // TODO: prompt about overriding
+    config.update('explorer.experimental.fileNesting.patterns', patterns, true)
+    window.showInformationMessage('Updated with latest patterns')
+  }
 }
 
 export function deactivate() {}
