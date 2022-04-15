3ca4131df945e9670b81055709ae76e177273065
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-04-14 17:49:33 -0700

    add compile_commands.json database under .clang-tidy

--- update.mjs
+++ update.mjs
@@ -268,7 +268,7 @@ const base = {
   'flake.nix': 'flake.lock',
   'BUILD.bazel': '*.bzl, *.bazel, *.bazelrc, bazel.rc, .bazelignore, .bazelproject, WORKSPACE',
   'CMakeLists.txt': '*.cmake, *.cmake.in, .cmake-format.yaml, CMakePresets.json',
-  '.clang-tidy': '.clang-format, .clangd',
+  '.clang-tidy': '.clang-format, .clangd, compile_commands.json',
   '*.pubxml': '$(capture).pubxml.user',
   '*.asax': '$(capture).*.cs, $(capture).*.vb',
   '*.ascx': '$(capture).*.cs, $(capture).*.vb',
