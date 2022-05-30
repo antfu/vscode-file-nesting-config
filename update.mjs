12f7f32f3d2e45ad35c2e7f81a8d32e49f1c835e
Muhammad Amin Saffari Taheri <48868969+MAST1999@users.noreply.github.com>
2022-05-30 16:11:56 +0430

    feat: add xaml

--- update.mjs
+++ update.mjs
@@ -306,6 +306,7 @@ const base = {
   '*.ashx': '$(capture).*.cs, $(capture).*.vb',
   '*.aspx': '$(capture).*.cs, $(capture).*.vb',
   '*.master': '$(capture).*.cs, $(capture).*.vb',
+  '*.xaml': '$(capture).xaml.cs',
   '*.resx': '$(capture).*.resx, $(capture).designer.cs, $(capture).designer.vb',
   '*.dart': '$(capture).freezed.dart, $(capture).g.dart',
   '*.module.ts': '$(capture).resolver.ts, $(capture).controller.ts, $(capture).service.ts',
