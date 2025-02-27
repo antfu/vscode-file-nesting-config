9de6cc6b100f66b54ef61446ee51fd432cf84f57
Brad Zacher <brad.zacher@gmail.com>
2025-02-28 09:54:51 +1030

    add .bazelversion to BUILD.bazel
    https://bazel.build/install/bazelisk#manage-with-bazelisk
    
    This file is used by bazelisk to auto-manage your bazel version

--- update.mjs
+++ update.mjs
@@ -480,7 +480,7 @@ const base = {
   '*.xaml': '$(capture).xaml.cs',
   'ansible.cfg': 'ansible.cfg, .ansible-lint, requirements.yml',
   'build-wrapper.log': 'build-wrapper*.log, build-wrapper-dump*.json, build-wrapper-win*.exe, build-wrapper-linux*, build-wrapper-macosx*',
-  'BUILD.bazel': '*.bzl, *.bazel, *.bazelrc, bazel.rc, .bazelignore, .bazelproject, WORKSPACE',
+  'BUILD.bazel': '*.bzl, *.bazel, *.bazelrc, bazel.rc, .bazelignore, .bazelproject, .bazelversion, WORKSPACE',
   'CMakeLists.txt': '*.cmake, *.cmake.in, .cmake-format.yaml, CMakePresets.json, CMakeCache.txt',
   'default.nix': 'shell.nix',
   'flake.nix': 'flake.lock',
