2c64bdbbd341bd69707e8c34c4b0c5ae62af2185
Jack-sh1 <shennalin2@gmail.com>
2026-04-21 15:48:19 +0800

    feat: add justfile nesting support
    Closes #293

--- update.mjs
+++ update.mjs
@@ -557,6 +557,7 @@ const base = {
   'go.mod': 'go.sum',
   'go.work': 'go.work.sum',
   'I*.cs': '$(capture).cs',
+  'justfile': '*.just, .justfile',
   'Makefile': '*.mk',
   'pom.xml': 'mvnw*',
   'shims.d.ts': '*.d.ts',
